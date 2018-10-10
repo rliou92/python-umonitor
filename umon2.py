d627381714a8d03a2e3509b71534e060f14d167f
Ricky Liou <rliou92@gmail.com>
2018-10-09 23:34:36 -0700

    Starting to work on arg parsing

--- umon2.py
+++ umon2.py
@@ -1,11 +1,17 @@
+#!/usr/bin/python
+
 # from screen import Screen
 from conf_manager import ConfManager
 import logging
+import argparse
 
 # setup = current state of monitors, their resolutions, positions, etc
 # profile = loaded from configuration file
 
 # PYTHONMALLOC=malloc valgrind --leak-check=full --show-leak-kinds=definite python umon2.py
+
+parser = argparse.ArgumentParser(description="Manage monitor configuration.")
+parser.add_argument("save", help="saves current setup into configuration file")
 
 logging.basicConfig(level=logging.DEBUG)
 config_file = "umon2.conf"
