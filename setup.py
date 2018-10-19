baa793c3f102593509b7b1983b285871b08b9e32
Ricky Liou <rliou92@gmail.com>
2018-10-18 22:13:55 -0700

    Add additional information to setup.py

--- setup.py
+++ setup.py
@@ -14,6 +14,12 @@ setup(
 	name = "umonitor",
 	packages = find_packages(),
 	ext_modules = cythonize(extensions),
+	license="MIT",
+	version="20181018",
+	author="Ricky Liou",
+	author_email="rliou92@gmail.com",
+	description="Manage monitor configuration automatically.",
+	url="https://github.com/rliou92/python-umonitor",
 
 	zip_safe=False
 
